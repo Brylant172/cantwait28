75af63e3f54521c68ad6d48ae2869f34c86efdc5
Adam Smaka <adam@smaka.dev>
2022-01-09 11:01:54 +0200

    Add date picker to the adding screen

--- lib/features/add/page/add_page.dart
+++ lib/features/add/page/add_page.dart
@@ -2,11 +2,21 @@ import 'package:cantwait28/features/add/cubit/add_cubit.dart';
 import 'package:cantwait28/models/item_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
-class AddPage extends StatelessWidget {
+class AddPage extends StatefulWidget {
   const AddPage({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<AddPage> createState() => _AddPageState();
+}
+
+class _AddPageState extends State<AddPage> {
+  String? _imageURL;
+  String? _title;
+  DateTime? _releaseDate;
 
   @override
   Widget build(BuildContext context) {
@@ -42,13 +52,26 @@ class AddPage extends StatelessWidget {
                             ),
                           );
                     },
-                    icon: const Icon(
-                      Icons.check,
-                    ),
+                    icon: const Icon(Icons.check),
                   ),
                 ],
               ),
-              body: const _AddPageBody(),
+              body: _AddPageBody(
+                onTitleChanged: (newValue) {
+                  _title = newValue;
+                },
+                onImageUrlChanged: (newValue) {
+                  _imageURL = newValue;
+                },
+                onDateChanged: (newValue) {
+                  setState(() {
+                    _releaseDate = newValue;
+                  });
+                },
+                selectedDateFormatted: _releaseDate != null
+                    ? DateFormat.yMMMMd().format(_releaseDate!)
+                    : null,
+              ),
             );
           },
         ),
@@ -60,7 +83,16 @@ class AddPage extends StatelessWidget {
 class _AddPageBody extends StatelessWidget {
   const _AddPageBody({
     Key? key,
+    required this.onTitleChanged,
+    required this.onImageUrlChanged,
+    required this.onDateChanged,
+    this.selectedDateFormatted,
   }) : super(key: key);
+
+  final Function(String) onTitleChanged;
+  final Function(String) onImageUrlChanged;
+  final Function(DateTime?) onDateChanged;
+  final String? selectedDateFormatted;
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +101,27 @@ class _AddPageBody extends StatelessWidget {
         horizontal: 30,
         vertical: 20,
       ),
-      children: const [],
+      children: [
+        TextField(
+          onChanged: onTitleChanged,
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final selectedDate = await showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime.now().subtract(
+                const Duration(days: 365 * 10),
+              ),
+              lastDate: DateTime.now().add(
+                const Duration(days: 365 * 10),
+              ),
+            );
+            onDateChanged(selectedDate);
+          },
+          child: Text(selectedDateFormatted ?? 'Choose release date'),
+        ),
+      ],
     );
   }
 }
