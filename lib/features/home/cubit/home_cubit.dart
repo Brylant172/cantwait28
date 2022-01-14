821a839928e84d7b030fa99969ab12237122be3e
Adam Smaka <adam@smaka.dev>
2022-01-14 10:54:12 +0200

    Implement Item Model

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:cantwait28/models/item_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 part 'home_state.dart';
@@ -14,9 +15,18 @@ class HomeCubit extends Cubit<HomeState> {
     _streamSubscription = FirebaseFirestore.instance
         .collection('items')
         .orderBy('release_date')
-        .snapshots().listen(
+        .snapshots()
+        .listen(
       (items) {
-        emit(HomeState(items: items));
+        final itemModels = items.docs.map((doc) {
+          return ItemModel(
+            id: doc.id,
+            title: doc['title'],
+            imageURL: doc['image_url'],
+            relaseDate: (doc['release_date'] as Timestamp).toDate(),
+          );
+        }).toList();
+        emit(HomeState(items: itemModels));
       },
     )..onError(
         (error) {
--- lib/features/home/cubit/home_state.dart
+++ lib/features/home/cubit/home_state.dart
@@ -2,11 +2,12 @@ part of 'home_cubit.dart';
 
 class HomeState {
   const HomeState({
-    this.items,
+    this.items = const [],
     this.loadingErrorOccured = false,
     this.removingErrorOccured = false,
   });
-  final QuerySnapshot<Map<String, dynamic>>? items;
+
+  final List<ItemModel> items;
   final bool loadingErrorOccured;
   final bool removingErrorOccured;
 }
