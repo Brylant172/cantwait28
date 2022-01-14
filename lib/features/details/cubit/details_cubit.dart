034e177e00fd4c0400782ea04219820a7925bb4d
Adam Smaka <adam@smaka.dev>
2022-01-14 14:26:09 +0200

    Add Details page

---
+++ lib/features/details/cubit/details_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:cantwait28/models/item_model.dart';
+import 'package:cantwait28/repositories/items_repository.dart';
+
+part 'details_state.dart';
+
+class DetailsCubit extends Cubit<DetailsState> {
+  DetailsCubit(this._itemsRepository) : super(DetailsState(itemModel: null));
+
+  final ItemsRepository _itemsRepository;
+
+  Future<void> getItemWithID(String id) async {
+    final itemModel = await _itemsRepository.get(id: id);
+    emit(DetailsState(itemModel: itemModel));
+  }
+}
