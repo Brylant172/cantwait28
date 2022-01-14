4fc806a4a56d86119c3422b0d9762f945fc8ce24
Adam Smaka <adam@smaka.dev>
2022-01-14 15:26:02 +0200

    Add flutterfire_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cantwait28/features/home/pages/home_page.dart';
+import 'package:cantwait28/features/auth/pages/auth_gate.dart';
 import 'package:cantwait28/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const HomePage(),
+      home: const AuthGate(),
     );
   }
 }
