e572f016951ddb03a7e0ef2c3e23c94e16e15066
Kacper Kłosiński <k.klosinski91@gmail.com>
2022-12-12 20:36:22 +0100

    Pop Add Screen on save

--- lib/features/add/page/add_page.dart
+++ lib/features/add/page/add_page.dart
@@ -18,50 +18,57 @@ class _AddPageState extends State<AddPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AddCubit(),
-      child: BlocBuilder<AddCubit, AddState>(
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Add new upcoming title'),
-              actions: [
-                IconButton(
-                  onPressed: _imageURL == null ||
-                          _title == null ||
-                          _releaseDate == null
-                      ? null
-                      : () {
-                          context.read<AddCubit>().add(
-                                _title!,
-                                _imageURL!,
-                                _releaseDate!,
-                              );
-                        },
-                  icon: const Icon(Icons.check),
-                ),
-              ],
-            ),
-            body: _AddPageBody(
-              onTitleChanged: (newValue) {
-                setState(() {
-                  _title = newValue;
-                });
-              },
-              onImageUrlChanged: (newValue) {
-                setState(() {
-                  _imageURL = newValue;
-                });
-              },
-              onDateChanged: (newValue) {
-                setState(() {
-                  _releaseDate = newValue;
-                });
-              },
-              selectedDateFormatted: _releaseDate?.toIso8601String(),
-            ),
-          );
-        },
+    return BlocListener<AddCubit, AddState>(
+      listener: (context, state) {
+        if (state.saved) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: BlocProvider(
+        create: (context) => AddCubit(),
+        child: BlocBuilder<AddCubit, AddState>(
+          builder: (context, state) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text(state.saved ? 'TRUE' : 'FALSE'),
+                actions: [
+                  IconButton(
+                    onPressed: _imageURL == null ||
+                            _title == null ||
+                            _releaseDate == null
+                        ? null
+                        : () {
+                            context.read<AddCubit>().add(
+                                  _title!,
+                                  _imageURL!,
+                                  _releaseDate!,
+                                );
+                          },
+                    icon: const Icon(Icons.check),
+                  ),
+                ],
+              ),
+              body: _AddPageBody(
+                onTitleChanged: (newValue) {
+                  setState(() {
+                    _title = newValue;
+                  });
+                },
+                onImageUrlChanged: (newValue) {
+                  setState(() {
+                    _imageURL = newValue;
+                  });
+                },
+                onDateChanged: (newValue) {
+                  setState(() {
+                    _releaseDate = newValue;
+                  });
+                },
+                selectedDateFormatted: _releaseDate?.toIso8601String(),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
